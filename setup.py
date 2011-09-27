7c3e986ca09313353d4c68b27a689ae0e0e0a595
Joe Jasinski <joe.jasinski@gmail.com>
2011-09-26 20:10:14 -0500

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='p2p',
+      version='0.1',
+      description='Peer 2 Peer',
+      author='Joe Jasinski',
+      url='http://www.joejasinski.com',
+     )

2b51efa6e84e312ea5443c100b0f44bc8b0df704
Joe Jasinski <joe.jasinski@gmail.com>
2011-09-27 22:14:32 -0500

    Added URL persistance model

--- birdflew/api/views.py
+++ birdflew/api/views.py
@@ -8,6 +8,8 @@ from django.contrib.sites.models import Site
 from django.conf import settings
 from django.views.generic.base import View
 
+from bcore.models import UrlModel
+
 from lxml import etree
 from lxml.builder import ElementMaker 
 
@@ -21,24 +23,32 @@ def prepxml(xml, status):
 
 class BlankView(View):
     
+    def _xml_error(self, message):
+        E = ElementMaker()
+        MESSAGE = E.message  
+        ERROR = E.error
+        xml = MESSAGE(ERROR(message))
+        return etree.tostring(xml)
+        
+    
     def get(self, request, *args, **kwargs):
-        return HttpResponse('Method not allowed', status=405)
+        return HttpResponse(self._xml_error('Method not allowed'), status=405)
 
     def post(self, request, *args, **kwargs):
-        return HttpResponse('Method not allowed', status=405)
+        return HttpResponse(self._xml_error('Method not allowed'), status=405)
 
     def put(self, request, *args, **kwargs):
-        return HttpResponse('Method not allowed', status=405)
+        return HttpResponse(self._xml_error('Method not allowed'), status=405)
 
     def delete(self, request, *args, **kwargs):
-        return HttpResponse('Method not allowed', status=405)
+        return HttpResponse(self._xml_error('Method not allowed'), status=405)
 
 
 class lookupurlsView(BlankView):
 
     def get(self, request, *args, **kwargs):
     
-        url_list = ['10.0.0.1','10.0.0.2',]
+        url_list = UrlModel.objects.all()
         E = ElementMaker()
         URLS = E.urls
         URL = E.url
---
+++ birdflew/bcore/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class TrackingMixin(models.Model):
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UrlModel(TrackingMixin):
+    
+    url = models.TextField()
+    

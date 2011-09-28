921331c6ab6891534da7ec65edf579325d932d02
Joe Jasinski <joe.jasinski@gmail.com>
2011-09-27 21:53:36 -0500

    remove piston as requirement. refactored views

--- birdflew/api/urls.py
+++ birdflew/api/urls.py
@@ -1,26 +1,19 @@
 from django.conf.urls.defaults import patterns, include, url
-
-from piston.resource import Resource
-from birdflew.api.handlers import ( 
-    LookupURLsHandler, RegisterURLsHandler, WhoamiHandler, LookupURLsHandler ) 
-
-ad = {}
-
-lookupurls_handler = Resource(handler=LookupURLsHandler, **ad)
-registerurls_handler = Resource(handler=RegisterURLsHandler, **ad)
-whoami_handler = Resource(handler=WhoamiHandler, **ad)
-logs_handler = Resource(handler=LookupURLsHandler, **ad)
-
+from . import views
 
 version_one_url = patterns('',
-    url(r'^lookupurls/$', lookupurls_handler, {'emitter_format':'xml',}, 
+
+    url(r'^lookupurls/$', views.lookupurlsView.as_view(), {}, 
        name='api_lookupurls'),
-    url(r'^registerurls/$', registerurls_handler, {'emitter_format':'xml',}, 
+
+    url(r'^registerurls/$', views.registerurlsView.as_view(), {}, 
        name='api_registerurls'),
-    url(r'^whoami/$', registerurls_handler, {'emitter_format':'xml', }, 
+
+    url(r'^whoami/$', views.whoamiView.as_view(), {'emitter_format':'xml', }, 
        name='api_whoami'),
-    url(r'^logs/$', logs_handler, {'emitter_format':'xml', }, 
-       name='api_logs'),
+
+#    url(r'^logs/$', logs_handler, {'emitter_format':'xml', }, 
+#       name='api_logs'),
 )
 
 urlpatterns = patterns('',
--- birdflew/api/views.py
+++ birdflew/api/views.py
@@ -1,15 +1,67 @@
-#from django.shortcuts import render_to_response
-#from django.template import RequestContext, loader, Context
-#from django.core.urlresolvers import reverse
-#from django.contrib.auth.decorators import login_required
-#from django.http import HttpResponseRedirect, HttpResponse
-#from django.contrib.gis.measure import D
-#from django.contrib.sites.models import Site
-#from django.conf import settings
-#
-#
-#def get_socket(request):
-#    
-#    response = HttpResponse(template.render(c), content_type="application/xml")
-#    response['Content-Disposition'] = 'attachment; filename=test.xml'
-#    return response
+from django.shortcuts import render_to_response
+from django.template import RequestContext, loader, Context
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.gis.measure import D
+from django.contrib.sites.models import Site
+from django.conf import settings
+from django.views.generic.base import View
+
+from lxml import etree
+from lxml.builder import ElementMaker 
+
+def prepxml(xml, status):
+    
+    decl = """<?xml version="1.0" encoding="utf-8"?>"""
+    response = HttpResponse("%s%s" % (decl, xml), status=status, content_type="application/xml")
+    response['Content-Disposition'] = 'inline; filename=test.xml'
+    return response
+
+
+class BlankView(View):
+    
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Method not allowed', status=405)
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('Method not allowed', status=405)
+
+    def put(self, request, *args, **kwargs):
+        return HttpResponse('Method not allowed', status=405)
+
+    def delete(self, request, *args, **kwargs):
+        return HttpResponse('Method not allowed', status=405)
+
+
+class lookupurlsView(BlankView):
+
+    def get(self, request, *args, **kwargs):
+    
+        url_list = ['10.0.0.1','10.0.0.2',]
+        E = ElementMaker()
+        URLS = E.urls
+        URL = E.url
+        status=200
+        xml = URLS(*map(lambda x: URL(x), url_list))
+        return prepxml(etree.tostring(xml), status)
+
+
+class registerurlsView(BlankView):
+
+    def post(self, request, *args, **kwargs):
+    
+        url_list = ['10.0.0.1','10.0.0.2',]
+    
+        E = ElementMaker()
+        URLS = E.urls
+        URL = E.url
+        status=201
+        xml = URLS(*map(lambda x: URL(x), url_list))
+        return prepxml(etree.tostring(xml), status)
+
+
+class whoamiView(BlankView):
+
+    def get(self, request, *args, **kwargs):
+         return HttpResponse("joe.jasinski+dp@gmail.com", status=200)

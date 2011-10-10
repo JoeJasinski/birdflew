b03ecafdfd629c6e6aa079ca5c2faf81c299ade3
Joe Jasinski <joe.jasinski@gmail.com>
2011-10-10 01:05:19 -0500

    fix urls

--- birdflew/api/validators.py
+++ birdflew/api/validators.py
@@ -1,7 +1,9 @@
 import re
 from urlparse import urlparse
 from lxml import etree
-
+from django.core.cache import cache
+from django.http import HttpResponseRedirect, HttpResponse
+from django.conf import settings
 
 input_message_spec = """<element name="urls" xmlns="http://relaxng.org/ns/structure/1.0">
   <oneOrMore>
@@ -35,6 +37,8 @@ def validate_url_format(url):
 
     # normalize the output
     messages = []
+    domain = ""
+    port = ""
     m = re.compile('^http[s]?://')
     if not m.match(url):
         url = "http://%s" % url
@@ -61,4 +65,40 @@ def validate_url_format(url):
         if not port:
             port = 80
     
+    url = "%s%s:%s" % (up.schema, domain, port)
+    
     return (url, domain, port, messages)
+
+
+class RateLimitDecorator(object):
+    def __init__(self, orig_func):
+        self.orig_func = orig_func
+        self.__name__ = orig_func.__name__
+        
+    __name__ = 'rate_limit'
+
+    def __call__(self,  *args, **kwargs):
+        # influenced by http://charlesleifer.com/blog/django-patterns-view-decorators/
+        request = args[0]
+        remote_addr = request.META.get('REMOTE_ADDR')
+        key = '%s.%s' % (remote_addr, request.get_full_path())
+        allow = True
+        view_count = cache.get(key, '')
+        if view_count:
+            if view_count >= settings.DEFAULT_MAX_REQUESTS_PER_INTERVAL:
+                allow = False
+            else:
+                allow = True
+                cache.incr(key)
+        else:
+            cache.set(key, 1, settings.DEFAULT_CACHE_RATE_LIMIT)
+            allow = True
+        
+        if not allow:
+            response = HttpResponse("throttled", status=403, content_type="text/plain")
+            response['Content-Disposition'] = 'inline; filename=test.txt'
+            return response
+        
+        return self.orig_func(*args, **kwargs)
+    
+    

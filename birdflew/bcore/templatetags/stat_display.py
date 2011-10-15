7f9717c6f1b8a8da799a30d1ed0ce166fb919429
Joe Jasinski <joe.jasinski@gmail.com>
2011-10-15 17:58:06 -0500

    added color to status

---
+++ birdflew/bcore/templatetags/stat_display.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='status_color')
+def status_color(value, *args):
+    return_value = ""
+    color_map = {8:"ff6600", 7:"ff9900", 6:"FFCC00", 5:"FFFF00", 
+     4:"ccff00", 3:"99ff00", 2:"66ff00", 1:"33ff00", 0:"00FF00"}
+    if value > 8:
+        return_value = "ff6600"
+    else:
+        return_value = (color_map.get(value) or 'fff')
+    return return_value

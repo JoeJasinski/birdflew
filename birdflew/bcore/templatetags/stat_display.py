ad45f8ac454fb4f5a190ec00ec1d6b86b487f21d
Joe Jasinski <joe.jasinski@gmail.com>
2011-10-15 18:24:33 -0500

    colors

--- birdflew/bcore/templatetags/stat_display.py
+++ birdflew/bcore/templatetags/stat_display.py
@@ -8,7 +8,7 @@ def status_color(value, *args):
     color_map = {10:"FF0000", 9:"FF3300", 8:"ff6600", 7:"ff9900", 
                  6:"FFCC00", 5:"FFFF00", 4:"ccff00", 3:"99ff00", 
                  2:"66ff00", 1:"33ff00", 0:"00FF00"}
-    if value > 8:
+    if value > 10:
         return_value = "ff6600"
     else:
         return_value = (color_map.get(value) or 'fff')

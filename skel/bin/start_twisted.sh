48a3b606dbc2ffc710283ef573a3a2f139fdba11
Joe Jasinski <joe@domU-12-31-39-04-1C-C6.compute-1.internal>
2011-10-15 21:15:40 +0000

    run twisted in background

--- skel/bin/start_twisted.sh
+++ skel/bin/start_twisted.sh
@@ -6,5 +6,5 @@ ENV_SETTINGS="birdflew.settings"
 export PYTHONPATH="$PYTHONPATH:${VIRTUAL_ENV}/proj/:${VIRTUAL_ENV}/proj/${MAIN_MODULE}/"
 
 echo "Starting Client..."
-env python ${VIRTUAL_ENV}/bin/django-admin.py bf_run_client --settings=${ENV_SETTINGS} 
+env python ${VIRTUAL_ENV}/bin/django-admin.py bf_run_client --settings=${ENV_SETTINGS} & 
 echo "Started Client:  " 

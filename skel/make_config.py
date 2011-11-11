caada435df34c5a622f65ba4e348387fe122edb2
Joe Jasinski <joe.jasinski@gmail.com>
2011-11-11 05:39:23 +0000

    update skemakre to write files

--- skel/make_config.py
+++ skel/make_config.py
@@ -8,14 +8,14 @@ env = Environment(loader=FileSystemLoader(os.path.join(VIRTUAL_ENV, 'proj', 'ske
 SHARED = {'ENVIRONMENT_DIR':VIRTUAL_ENV, 'APP_NAME':'birdflew', 'APP_USER':'p2p', 'WEB_USER':'www-data', 'GROUP':'worker'}
 
 mapping = [ 
-('nginx/nginx.conf', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'nginx.conf'), dict({}, **SHARED)),
-('nginx/server.conf', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'server.conf'), dict({}, **SHARED)),
-('nginx/locations.conf', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'locations.conf'), dict({}, **SHARED)),
-('nginx/django.conf', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'django.conf'), dict({}, **SHARED)),
-('nginx/conf/fastcgi.conf', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'conf','fastcgi.conf'), dict({}, **SHARED)),
-('nginx/conf/mime.types', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'conf','mime.types'), dict({}, **SHARED)),
-('nginx/conf/proxy.conf', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'conf','proxy.conf'), dict({}, **SHARED)),
-('redis/redis.conf', os.path.join(VIRTUAL_ENV, 'etc', 'redis', 'redis.conf'), dict({}, **SHARED)),
+#('nginx/nginx.conf', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'nginx.conf'), dict({}, **SHARED)),
+#('nginx/server.conf', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'server.conf'), dict({}, **SHARED)),
+#('nginx/locations.conf', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'locations.conf'), dict({}, **SHARED)),
+#('nginx/django.conf', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'django.conf'), dict({}, **SHARED)),
+#('nginx/conf/fastcgi.conf', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'conf','fastcgi.conf'), dict({}, **SHARED)),
+#('nginx/conf/mime.types', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'conf','mime.types'), dict({}, **SHARED)),
+#('nginx/conf/proxy.conf', os.path.join(VIRTUAL_ENV, 'etc', 'nginx', 'conf','proxy.conf'), dict({}, **SHARED)),
+#('redis/redis.conf', os.path.join(VIRTUAL_ENV, 'etc', 'redis', 'redis.conf'), dict({}, **SHARED)),
 ('bin/start_fastcgi.sh', os.path.join(VIRTUAL_ENV, 'bin', 'start_fastcgi.sh'), dict({}, **SHARED)),
 ('bin/start_nginx.sh', os.path.join(VIRTUAL_ENV, 'bin', 'start_nginx.sh'), dict({}, **SHARED)),
 ('bin/start_twisted.sh', os.path.join(VIRTUAL_ENV, 'bin', 'start_twisted.sh'), dict({}, **SHARED)),
@@ -28,4 +28,7 @@ for source, dest, context in mapping:
     print "======================================================="
     print "== %s" % dest
     print "========================================================"
-    print template.render(**context)
+    rendered_template = template.render(**context)
+    f = open(dest, 'w')
+    f.write(rendered_template)
+    f.close()
